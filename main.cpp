c55cbfc6dd17f66f6466e3e3af79e00b9dd47b2a
junios <junios@naver.com>
2024-05-13 17:21:55 +0900

    플레이어 움직이기

--- main.cpp
+++ main.cpp
@@ -1,19 +1,80 @@
 #include <iostream>
-
+#include <conio.h>
+#include <winsock.h>
 using namespace std;
 
 int main()
 {
-	int	Number1 = 0;
-	int	Number2 = 0;
+	int Map[10][10]	=
+	{
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	};
 
-	cin >> Number1;
-	cin >> Number2; 
+	int PlayerX = 1;
+	int PlayerY = 1;
+	char Key = 0;
 
-	cout << Number1 + Number2 << endl;
-	cout << Number1 - Number2 << endl;
-	cout << Number1 / Number2 << endl;
-	cout << Number1 * Number2 << endl;
+	for (;;)
+	{
+		cin >> Key; //wasd
+		if (Key == 'w')
+		{
+			PlayerY--;
+		}
+		else if (Key == 's')
+		{
+			PlayerY++;
+		}
+		else if (Key == 'a')
+		{
+			PlayerX--;
+		}
+		else if (Key == 'd')
+		{
+			PlayerX++;
+		}
+
+		system("cls");
+		for (int Y = 0; Y < 10; ++Y)
+		{
+			for (int X = 0; X < 10; ++X)
+			{
+				if (PlayerX == X && PlayerY == Y)
+				{
+					cout << "P";
+				}
+				else if (Map[Y][X] == 0)
+				{
+					cout << " ";
+				}
+				else if (Map[Y][X] == 1)
+				{
+					cout << "*";
+				}
+			}
+			cout << endl;
+		}
+	}
+
+	//GameLoop
+	//for (;;) //Frame, 60FPS
+	//{
+	//	Input();
+	//	//Process();
+	//	Tick();
+	//	Render();
+	//}
+
+
 
 	return 0;
 }
